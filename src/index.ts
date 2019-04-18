680138099fe4604ff3349137e1ddf303619f5f20
ludw1gj <19744067+ludw1gj@users.noreply.github.com>
2019-04-18 20:40:20 +1000

    Refactored draw function's parameters into an options obj

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,34 @@ const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 const genRandomNum = (max: number, min: number): number =>
   parseFloat((Math.random() * (max - min) + min).toFixed(2));
 
-function draw(
-  startX: number,
-  startY: number,
-  len: number,
-  angle: number, // should be = 0 on start,
-  changeInAngel: number,
-  branchWidth: number,
-  lengthDegradation: number,
-  branchWidthDegradation: number
-): void {
+interface DrawOptions {
+  startX: number;
+  startY: number;
+  branchWidth: number;
+  branchWidthDegradation: number;
+  branchLength: number;
+  branchLengthDegradation: number;
+  angle?: number;
+  changeInAngel: number;
+  symmetrical: boolean;
+}
+
+function draw(options: DrawOptions): void {
+  if (!options.angle) {
+    options.angle = 0;
+  }
+  const {
+    startX,
+    startY,
+    branchWidth,
+    branchWidthDegradation,
+    branchLength,
+    branchLengthDegradation,
+    angle,
+    changeInAngel,
+    symmetrical,
+  } = options;
+
   ctx.lineWidth = branchWidth;
   ctx.beginPath();
   ctx.save();
@@ -23,60 +41,55 @@ function draw(
   ctx.translate(startX, startY);
   ctx.rotate((angle * Math.PI) / 180);
   ctx.moveTo(0, 0);
-  ctx.lineTo(0, -len);
+  ctx.lineTo(0, -length);
   ctx.stroke();
 
-  if (len < 15) {
+  if (length < 15) {
     ctx.restore();
     return;
   }
 
-  // remove 2 lines
-  lengthDegradation = genRandomNum(0.85, 0.8);
-  branchWidthDegradation = genRandomNum(0.8, 0.7);
-  changeInAngel = genRandomNum(30, 10);
+  if (!symmetrical) {
+    options.branchLengthDegradation = genRandomNum(0.85, 0.8);
+    options.branchWidthDegradation = genRandomNum(0.8, 0.7);
+    options.changeInAngel = genRandomNum(30, 10);
+  }
 
-  draw(
-    0,
-    -len,
-    len * lengthDegradation,
-    -changeInAngel,
-    changeInAngel,
-    branchWidth * branchWidthDegradation,
-    lengthDegradation,
-    branchWidthDegradation
-  );
-  draw(
-    0,
-    -len,
-    len * lengthDegradation,
-    changeInAngel,
-    changeInAngel,
-    branchWidth * branchWidthDegradation,
-    lengthDegradation,
-    branchWidthDegradation
-  );
+  const drawOptionsShared = {
+    ...options,
+    startX: 0,
+    startY: -branchLength,
+  };
 
+  const drawOptionsFirstChild = {
+    ...drawOptionsShared,
+    branchLength: branchLength * branchLengthDegradation,
+    branchWidth: branchWidth * branchWidthDegradation,
+    angle: -changeInAngel,
+  };
+
+  const drawOptionsSecondChild = {
+    ...drawOptionsShared,
+    branchLength: branchLength * branchLengthDegradation,
+    branchWidth: branchWidth * branchWidthDegradation,
+    angle: -changeInAngel,
+  };
+
+  draw(drawOptionsFirstChild);
+  draw(drawOptionsSecondChild);
   ctx.restore();
 }
 
-const randomLength = genRandomNum(120, 100);
-const randomWidth = genRandomNum(15, 5);
+const options = {
+  startX: 500,
+  startY: 700,
+  branchWidth: genRandomNum(15, 5),
+  branchWidthDegradation: genRandomNum(0.8, 0.7),
+  branchLength: genRandomNum(120, 100),
+  branchLengthDegradation: genRandomNum(0.85, 0.8),
+  angle: 0,
+  changeInAngel: genRandomNum(30, 10),
+  symmetrical: true,
+};
 
-const lengthDegradation = genRandomNum(0.85, 0.8);
-const branchWidthDegradation = genRandomNum(0.8, 0.7);
-const changeInAngel = genRandomNum(30, 10);
-
-draw(
-  500,
-  700,
-  randomLength,
-  0,
-  changeInAngel,
-  randomWidth,
-  lengthDegradation,
-  branchWidthDegradation
-);
-
-// simetric or not
-// binary fractal tree
+draw(options);
